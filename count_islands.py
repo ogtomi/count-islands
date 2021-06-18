21a47e54f476b9e3fb451a7d666a9d17114fc001
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2021-06-18 20:02:43 +0200

    Added islands counting from 2D array covering all the cases (including diagonal)

--- count_islands.py
+++ count_islands.py
@@ -1,17 +1,56 @@
-with open('example.txt', 'r') as f:
+path = 'D:\Random\count-islands\data.txt'
+with open(path, 'r') as f:
     content = f.read().split('\n')
 
-M = len(content[0])
-N = len(content) 
+def get_2d(data, M, N):
+    arr_2d = []
 
-count = 0
+    for i in range(N):
+        new = []
+        for j in range(M):
+            new.append(data[i][j])
+        arr_2d.append(new)
+    
+    return arr_2d
 
-for row in range(N):
-    for col in range(M):
-        if content[row][col] == 1:
-            count += 1
+def land_check(data, M, N, row_new, col_new):
+    if data[row_new][col_new] == '0':
+        return
 
-print(count)
-print("Lengths")
-print(M)
-print(N) 
+    data[row_new][col_new] = '0'
+
+    if row_new != 0:
+        land_check(data, M, N, row_new-1, col_new)
+        if col_new != M - 1:
+            land_check(data, M, N, row_new-1, col_new+1)
+    
+    if row_new != N - 1:
+        land_check(data, M, N, row_new+1, col_new)
+        if col_new != 0:
+            land_check(data, M, N, row_new+1, col_new-1)
+    
+    if col_new != 0:
+        land_check(data, M, N, row_new, col_new-1)
+        if row_new != 0:
+            land_check(data, M, N, row_new-1, col_new-1)
+    
+    if col_new != M - 1:
+        land_check(data, M, N, row_new, col_new+1)
+        if row_new != N - 1:
+            land_check(data, M, N, row_new+1, col_new+1)
+
+def count_islands(data):
+    M = len(data[0])
+    N = len(data)
+        
+    count = 0
+    data_2d = get_2d(data, M, N)
+
+    for row in range(N):
+        for col in range(M):
+            if data_2d[row][col] == '1':
+                land_check(data_2d, M, N, row, col)
+                count += 1
+    return count
+
+print(count_islands(content))
