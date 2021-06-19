82853afd05edb720a88c86302dc9730be8c203f8
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2021-06-19 23:15:55 +0200

    Added second test

--- test_count_islands.py
+++ test_count_islands.py
@@ -3,13 +3,18 @@ import count_islands
 
 class TestCountIslands (unittest.TestCase):
     def setUp(self):
-        self.arr_2d = ['000000000', '010000000', '111000100', '110001110', '000001100']
-        self.rows = len(self.arr_2d)
-        self.cols = len(self.arr_2d[0])
+        self.arr = ['000000000', '010000000', '111000100', '110001110', '000001100', '001000000', '110000000', '000001100']
+        self.rows = len(self.arr)
+        self.cols = len(self.arr[0])
+        self.counter = 4
 
     def test_get_2d(self):
-        array = count_islands.get_2d(self.arr_2d, self.rows, self.cols)
+        array = count_islands.get_2d(self.arr, self.rows, self.cols)
         self.assertTrue(type(array) is list)
+
+    def test_count_islands(self):
+        result = count_islands.count_islands(self.arr)
+        self.assertEqual(result, self.counter)
 
 if __name__ == '__main__':
     unittest.main()
